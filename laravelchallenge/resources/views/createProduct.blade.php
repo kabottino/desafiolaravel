2be0df409dbb12357827346ecfea32397c48d65b
kabottino <kabottino@hotmail.com>
2020-03-15 13:37:55 -0300

    adicionando o require head

---
+++ laravelchallenge/resources/views/createProduct.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require ('./includes/navbar.php'); ?>
+<body>
+    
+</body>
+</html>
--- laravelchallenge/resources/views/home.blade.php
+++ laravelchallenge/resources/views/home.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio</title>
-</head>
+<?php require ('./includes/navbar.php'); ?>
 <body>
-    
+    Teste
 </body>
 </html>
